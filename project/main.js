c9f3b0cd88e297acdfeb708a7eea6ca0c80a25e5
Илья Мелеховец <>
2019-02-19 00:48:38 +0300

    2 hours 20 min fixed code

--- project/main.js
+++ project/main.js
@@ -1,15 +1,22 @@
-$('.tag').click(function(e) {
-    e.preventDefault();
-    $('.tag').removeClass('active-tag');
-    $(this).toggleClass('active-tag');
-    var content = $(this).text();
-    $('.tag-label').text(content);
-    $('.tag-list').toggleClass('hidden');
+$(document).ready(function () {
+    $('.tag').click(function(e) {
+        e.preventDefault();
+        $('.tag').removeClass('active-tag');
+        $(this).toggleClass('active-tag');
+        var content = $(this).text();
+        $('.tag-label').text(content);
+        $('.tag-list').toggleClass('hidden');
+    })
+    $('.tag-label').click(function(e) {
+        e.preventDefault();
+        $('.tag-list').toggleClass('hidden');
+    })
+    $('.question__link').click(function(e) {
+        e.preventDefault();
+        $(this).toggleClass('visible').next().slideToggle();
+    })
 })
-$('.tag-label').click(function(e) {
-    e.preventDefault();
-    $('.tag-list').toggleClass('hidden');
-})
+
 
 
 
